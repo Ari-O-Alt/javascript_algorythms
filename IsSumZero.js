8ec4795e30f6aac2a5305545a0467e91a95affe9
Silvia Bogdan <titifursec@outlook.com>
2021-02-13 12:32:49 +0100

    Added explanations and comments to IsSumZero

--- IsSumZero.js
+++ IsSumZero.js
@@ -1,24 +1,32 @@
-/* The purpose of this program is to find out if an array contains any numbers that sum up to zero. We'll use as refrence points, two numbers in the array, the first and the last and change their index depensing on some conditions. */
+/* The purpose of this program is to find out if an array contains any two numbers that sum up to zero. We'll use as refrence points, two numbers in the array, the first and the last and change their index depending on if the numbers have been found already or not. If not two numbers sum up to zero, we'll print the message: "No two numbers in this array sum up to zero!" */
 
 const numbersArray = [-4, -2, 0, 4, 9];
 
 const isSumZero = (array) => {
-  // we set
+  // we set the first number's index
   let leftIndex = 0;
+  //we set the second's number's index
   let rightIndex = array.length - 1;
 
+  //we continue to calculate/ check for things while the leftIndex(number) is smaller than the rigthIndex(number)
   while (leftIndex < rightIndex) {
+    // we add the two numbers together
     let sum = array[leftIndex] + array[rightIndex];
+    // if the sum of the two numbers is 0, add them to an array an terminate
     if (sum === 0) {
       const result = [array[leftIndex], array[rightIndex]];
       console.log(result);
       return result;
+      // if the sum is bigger than zero, shift the index of the number on the right and calculate again
     } else if (sum > 0) {
       rightIndex--;
+      // else, shift the index of the numberon the left and calculate again
     } else {
       leftIndex++;
     }
   }
+  console.log('No two numbers in this array sum up to zero!');
 };
 
 isSumZero(numbersArray);
+isSumZero([-4, -2, 0, 5, 9]);
